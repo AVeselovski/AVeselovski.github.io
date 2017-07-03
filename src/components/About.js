676d4f8a128bb5bfd80c28f83ce312eb4b8cf521
Artur Veselovski <artur.veselovski@hotmail.fi>
2017-07-03 23:31:38 +0300

    added about

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,36 @@
+// libs
 import React from 'react';
+// assets
+import FaceImg from '../assets/face.png';
 
 export default class About extends React.Component {
     render() {
         return (
             <section className="about">
-                <div className="container">About</div>
+                <div className="container">
+                    <div className="info-container-1">
+                        <img src={FaceImg} alt={'My Face'} />
+                        <div className="basic-info">
+                            <h2>Welcome!</h2>
+                            <p>My name is Artur and I'm studying Media Technology at Jyväskylä University of Applied Sciences, 
+                            currently at my third academic year. My goal is to grow as a full-stack developer.</p>
+                        </div>
+                    </div>
+                    <div className="info-container-2">
+                        <p>I am very passionate about web/UI design and development. I enjoy both front-end and back-end programming. 
+                        In web-design I love simple, yet elegant and most importantly working solutions with attention to details. 
+                        I have experience with teamwork and so I'm comfortable working as in team as well as solo. 
+                        Here are some technologies I love using...</p>
+                    </div>
+                    <div className="info-container-3">
+                        <span className="devicons devicons-html5" title="HTML5" />
+                        <span className="devicons devicons-css3" title="CSS3" />
+                        <span className="devicons devicons-sass" title="Sass" />
+                        <span className="devicons devicons-javascript_shield" title="JavaScript" />
+                        <span className="devicons devicons-nodejs_small" title="NodeJS" />
+                        <span className="devicons devicons-react" title="ReactJS" />
+                    </div>
+                </div>
             </section>
         );
     }
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
 // libs
 import React from 'react';
+import ScrollableAnchor from 'react-scrollable-anchor';
 // components
 import Nav from './Nav';
-import Page from './Page';
+import Landing from './Landing';
+import About from './About';
+import Projects from './Projects';
+import CV from './CV';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <Nav />
-                <Page />
+                <div className="wrapper">
+                    <ScrollableAnchor id={'page-top'}>
+                        <Landing title={'ARTUR VESELOVSKI'} titleKicker={'ICT-Student / Media Technology'} />
+                    </ScrollableAnchor>
+                    <ScrollableAnchor id={'about'}>
+                        <About />
+                    </ScrollableAnchor>
+                    <ScrollableAnchor id={'projects'}>
+                        <Projects />
+                    </ScrollableAnchor>
+                    <ScrollableAnchor id={'cv'}>
+                        <CV />
+                    </ScrollableAnchor>
+                </div>
             </div>
         );
     }
