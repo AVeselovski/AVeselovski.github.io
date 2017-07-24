665708e9d32a1cb349822de3712921e773ff76c5
Artur Veselovski <artur.veselovski@hotmail.fi>
2017-07-24 21:49:39 +0300

    meta cleanup

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -7,7 +7,10 @@ export default class Meta extends React.Component {
             <Helmet>
                 <meta charSet="utf-8" />
                 <meta name="description" content="Artur Veselovski - media technology, my portfolio. Full-Stack developer, web design." />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+                <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <meta name="theme-color" content="#ffffff" />
                 <title>AV | Portfolio</title>
             </Helmet>
         );
