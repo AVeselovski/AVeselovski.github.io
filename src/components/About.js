e189c546a9eaeaedd7b2cb5be711769fe8b3d090
Artur Veselovski <artur.veselovski@hotmail.fi>
2017-07-27 23:10:03 +0300

    content update

--- src/components/About.js
+++ src/components/About.js
@@ -18,23 +18,23 @@ export default class About extends React.Component {
                     <div className="info-container-1">
                         <h2>Welcome!</h2>
                         <div className="basic-info">
-                            <p>My name is Artur and I'm studying Media Technology at Jyväskylä University of Applied Sciences, 
-                            currently at my third academic year. My goal is to grow as a full-stack developer.</p>
+                            <p>My name is Artur Veselovski and I'm studying Media Technology at Jyväskylä University of Applied Sciences (JAMK).  
+                            Currently I am at my third academic year.</p>
                         </div>
                     </div>
                     <div className="info-container-2">
-                        <p>I am very passionate about web/UI design and development. I enjoy both front-end and back-end programming. 
-                        In web-design I love simple, yet elegant and most importantly working solutions with attention to details. 
-                        I have experience with teamwork and so I'm comfortable working as in team as well as solo. 
-                        Here are some technologies I love using...</p>
+                        <p>I am very passionate about web/UI design, front-end and full-stack development. 
+                            In web-design I love simple, yet elegant and most importantly working solutions with attention to details.</p>
+                        <p>I have experience working as part of a team and I'm familiar with Agile & Lean methods. 
+                        I enjoy learning and trying out new technologies and here are some of the technologies I use frequently...</p>
                     </div>
                     <div className="info-container-3">
                         <span className="devicons devicons-html5" title="HTML5" style={{color: iconColors.html5}} />
                         <span className="devicons devicons-css3" title="CSS3" style={{color: iconColors.css3}} />
-                        <span className="devicons devicons-sass" title="Sass" style={{color: iconColors.sass}} />
                         <span className="devicons devicons-javascript_shield" title="JavaScript" style={{color: iconColors.JS}} />
                         <span className="devicons devicons-nodejs_small" title="NodeJS" style={{color: iconColors.node}} />
                         <span className="devicons devicons-react" title="ReactJS" style={{color: iconColors.react}} />
+                        <span className="devicons devicons-sass" title="Sass" style={{color: iconColors.sass}} />                        
                     </div>
                 </div>
             </section>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,13 +19,13 @@ export default class Projects extends React.Component {
                                 <CardBlock>
                                     <CardTitle>Dynamic To-Do</CardTitle>
                                     <CardText>Simple To-Do connected to REST API. Back-end built with Node, Express and MongoDB. 
-                                        Front-end built using ReactJS and Material UI.</CardText>
+                                        Front-end built with React and Material-UI.</CardText>
                                     <div className="badge-holder">
                                         <Badge pill>Node</Badge>
                                         <Badge pill>React</Badge>
                                         <Badge pill>Express</Badge>
                                         <Badge pill>MongoDB</Badge>
-                                        <Badge pill>Material UI</Badge>
+                                        <Badge pill>Material-UI</Badge>
                                     </div>
                                 </CardBlock>
                             </a>
@@ -34,13 +34,14 @@ export default class Projects extends React.Component {
                             <a href="https://wimmalab.github.io" target="_blank" rel="noopener noreferrer">
                                 <CardBlock>
                                     <CardTitle>WIMMA Lab</CardTitle>
-                                    <CardText>WIMMA Lab sites. The final site (2 person project) was built with React and deployed using gh-pages. 
-                                        Blog and temporary sites were built using Jekyll and Bootstrap.</CardText>
+                                    <CardText>During my internship at WIMMA Lab (2.5 months) I completed several projects. A temporary website for the organization 
+                                        and a blog, data visualization for another team and the final WIMMA Lab website (2 person project).</CardText>
                                     <div className="badge-holder">
                                         <Badge pill>Node</Badge>
                                         <Badge pill>React</Badge>
                                         <Badge pill>Jekyll</Badge>
                                         <Badge pill>Bootstrap</Badge>
+                                        <Badge pill>Sass</Badge>
                                     </div>
                                 </CardBlock>
                             </a>
@@ -52,7 +53,7 @@ export default class Projects extends React.Component {
                                     <CardText>A website for a personal trainer. COMING SOON.</CardText>
                                     <div className="badge-holder">
                                         <Badge pill>HTML5</Badge>
-                                        <Badge pill>CSS3</Badge>
+                                        <Badge pill>Sass</Badge>
                                         <Badge pill>Bootstrap</Badge>
                                     </div>
                                 </CardBlock>
@@ -64,11 +65,12 @@ export default class Projects extends React.Component {
                                 <CardBlock>
                                     <CardTitle>IoTitude</CardTitle>
                                     <CardText>Data visualization using IoTitude's REST API. This small project was done during WIMMA Lab 
-                                        by request of virtual company IoTitude for demoing their product.</CardText>
+                                        by request of virtual company IoTitude for demoing their IoT product.</CardText>
                                     <div className="badge-holder">
                                         <Badge pill>Node</Badge>
                                         <Badge pill>React</Badge>
                                         <Badge pill>ChartJS</Badge>
+                                        <Badge pill>Sass</Badge>
                                         <Badge pill>Material UI</Badge>
                                     </div>
                                 </CardBlock>
